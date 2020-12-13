816660b326da39d049c2529f4ca8fb971bc27b6b
Nana Janashia <nn_janashia@yahoo.com>
2020-12-13 13:30:21 +0100

    fix resource ref

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.cidr_blocks[1].cidr_block
     availability_zone = var.avail_zone
     tags = {
